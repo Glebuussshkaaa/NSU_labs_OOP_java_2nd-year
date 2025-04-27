f946bd99e59071348ae77a6ab321261c26e0bcbc
Gleb Gleb <g.estraikh@gmail.com>
2025-04-27 15:31:02 +0700

    add png

--- jpacman/src/main/java/com/glebestraikh/jpacman/GameResources.java
+++ jpacman/src/main/java/com/glebestraikh/jpacman/GameResources.java
@@ -7,9 +7,9 @@ public class GameResources {
     private Image wallImage;
     private Image blueGhostImage;
     private Image orangeUpGhostImage;
-    private Image orangeDownGhostImage;
-    private Image orangeRightGhostImage;
-    private Image orangeLeftGhostImage;
+    private Image orangeGhostDownImage;
+    private Image orangeGhostRightImage;
+    private Image orangeGhostLeftImage;
     private Image pinkGhostImage;
     private Image redGhostImage;
     private Image pelletImage;
@@ -25,22 +25,22 @@ public class GameResources {
 
     private void loadImages() {
         wallImage = new ImageIcon(getClass().getResource("/sprite/wall.png")).getImage();
-        blueGhostImage = new ImageIcon(getClass().getResource("/sprite/blueGhost.png")).getImage();
+        blueGhostImage = new ImageIcon(getClass().getResource("/sprite/blueGhost/blueGhostRight.png")).getImage();
 
         orangeUpGhostImage = new ImageIcon(getClass().getResource("/sprite/orangeGhost/orangeGhostRight.png")).getImage();
-        orangeDownGhostImage = new ImageIcon(getClass().getResource("/sprite/orangeGhost/orangeGhostRight.png")).getImage();
-        orangeLeftGhostImage = new ImageIcon(getClass().getResource("/sprite/orangeGhost/orangeGhostRight.png")).getImage();
-        orangeRightGhostImage = new ImageIcon(getClass().getResource("/sprite/orangeGhost/orangeGhostRight.png")).getImage();
+        orangeGhostDownImage = new ImageIcon(getClass().getResource("/sprite/orangeGhost/orangeGhostRight.png")).getImage();
+        orangeGhostLeftImage = new ImageIcon(getClass().getResource("/sprite/orangeGhost/orangeGhostRight.png")).getImage();
+        orangeGhostRightImage = new ImageIcon(getClass().getResource("/sprite/orangeGhost/orangeGhostRight.png")).getImage();
 
-        pinkGhostImage = new ImageIcon(getClass().getResource("/sprite/pinkGhost.png")).getImage();
-        redGhostImage = new ImageIcon(getClass().getResource("/sprite/redGhost.png")).getImage();
+        pinkGhostImage = new ImageIcon(getClass().getResource("/sprite/pinkGhost/pinkGhostRight.png")).getImage();
+        redGhostImage = new ImageIcon(getClass().getResource("/sprite/redGhost/redGhostRight.png")).getImage();
         pelletImage = new ImageIcon(getClass().getResource("/sprite/pellet.png")).getImage();
 
-        pacmanOpenUpImage = new ImageIcon(getClass().getResource("/sprite/pacmanUp.png")).getImage();
-        pacmanOpenDownImage = new ImageIcon(getClass().getResource("/sprite/pacmanDown.png")).getImage();
-        pacmanOpenLeftImage = new ImageIcon(getClass().getResource("/sprite/pacmanLeft.png")).getImage();
-        pacmanOpenRightImage = new ImageIcon(getClass().getResource("/sprite/pacmanRight.png")).getImage();
-        pacmanClosedImage = new ImageIcon(getClass().getResource("/sprite/pacmanClosed.png")).getImage();
+        pacmanOpenUpImage = new ImageIcon(getClass().getResource("/sprite/pacman/pacmanUp.png")).getImage();
+        pacmanOpenDownImage = new ImageIcon(getClass().getResource("/sprite/pacman/pacmanDown.png")).getImage();
+        pacmanOpenLeftImage = new ImageIcon(getClass().getResource("/sprite/pacman/pacmanLeft.png")).getImage();
+        pacmanOpenRightImage = new ImageIcon(getClass().getResource("/sprite/pacman/pacmanRight.png")).getImage();
+        pacmanClosedImage = new ImageIcon(getClass().getResource("/sprite/pacman/pacmanClosed.png")).getImage();
     }
 
     // Getters
@@ -54,9 +54,9 @@ public class GameResources {
 
     public Image getOrangeGhostImage(char direction) {
         if (direction == 'U') return orangeUpGhostImage;
-        if (direction == 'D') return orangeDownGhostImage;
-        if (direction == 'L') return orangeLeftGhostImage;
-        return orangeRightGhostImage;
+        if (direction == 'D') return orangeGhostDownImage;
+        if (direction == 'L') return orangeGhostLeftImage;
+        return orangeGhostRightImage;
     }
 
     public Image getPinkGhostImage() {
